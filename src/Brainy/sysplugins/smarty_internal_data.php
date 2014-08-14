137c63f05a486fcec963cf7f5c94883ef5dcd34a
Matt Basta <mbasta@box.com>
2014-08-14 15:41:14 -0700

    Deprecate append()

--- src/Brainy/sysplugins/smarty_internal_data.php
+++ src/Brainy/sysplugins/smarty_internal_data.php
@@ -156,6 +156,10 @@ class Smarty_Internal_Data
      * two numerically indexed arrays may cause values to overwrite each other
      * or result in non-sequential keys.
      *
+     * @deprecated append() and all of its derivative functions are deprecated
+     *             because they do not observe scoping rules, and their current
+     *             semantics prevent them from being changed in a backwards-
+     *             compatible way.
      * @param  array|string         $tpl_var the template variable name(s)
      * @param  mixed                $value   the value to append
      * @param  boolean              $merge   flag if array elements shall be merged
@@ -165,47 +169,29 @@ class Smarty_Internal_Data
         if (is_array($tpl_var)) {
             // $tpl_var is an array, ignore $value
             foreach ($tpl_var as $_key => $_val) {
-                if ($_key != '') {
-                    if (!isset($this->tpl_vars[$_key])) {
-                        $tpl_var_inst = $this->getVariable($_key, null, true, false);
-                        if ($tpl_var_inst instanceof Undefined_Smarty_Variable) {
-                            $this->tpl_vars[$_key] = new Smarty_variable(null);
-                        } else {
-                            $this->tpl_vars[$_key] = clone $tpl_var_inst;
-                        }
-                    }
-                    if (!(is_array($this->tpl_vars[$_key]->value) || $this->tpl_vars[$_key]->value instanceof ArrayAccess)) {
-                        settype($this->tpl_vars[$_key]->value, 'array');
-                    }
-                    if ($merge && is_array($_val)) {
-                        foreach ($_val as $_mkey => $_mval) {
-                            $this->tpl_vars[$_key]->value[$_mkey] = $_mval;
-                        }
-                    } else {
-                        $this->tpl_vars[$_key]->value[] = $_val;
-                    }
+                $this->append($_key, $_val, $merge);
+            }
+            return $this;
+        }
+
+        if ($tpl_var != '' && isset($value)) {
+            if (!isset($this->tpl_vars[$tpl_var])) {
+                $tpl_var_inst = $this->getVariable($tpl_var, null, true, false);
+                if ($tpl_var_inst instanceof Undefined_Smarty_Variable) {
+                    $this->tpl_vars[$tpl_var] = new Smarty_variable(null);
+                } else {
+                    $this->tpl_vars[$tpl_var] = clone $tpl_var_inst;
                 }
             }
-        } else {
-            if ($tpl_var != '' && isset($value)) {
-                if (!isset($this->tpl_vars[$tpl_var])) {
-                    $tpl_var_inst = $this->getVariable($tpl_var, null, true, false);
-                    if ($tpl_var_inst instanceof Undefined_Smarty_Variable) {
-                        $this->tpl_vars[$tpl_var] = new Smarty_variable(null);
-                    } else {
-                        $this->tpl_vars[$tpl_var] = clone $tpl_var_inst;
-                    }
+            if (!(is_array($this->tpl_vars[$tpl_var]->value) || $this->tpl_vars[$tpl_var]->value instanceof ArrayAccess)) {
+                settype($this->tpl_vars[$tpl_var]->value, 'array');
+            }
+            if ($merge && is_array($value)) {
+                foreach ($value as $_mkey => $_mval) {
+                    $this->tpl_vars[$tpl_var]->value[$_mkey] = $_mval;
                 }
-                if (!(is_array($this->tpl_vars[$tpl_var]->value) || $this->tpl_vars[$tpl_var]->value instanceof ArrayAccess)) {
-                    settype($this->tpl_vars[$tpl_var]->value, 'array');
-                }
-                if ($merge && is_array($value)) {
-                    foreach ($value as $_mkey => $_mval) {
-                        $this->tpl_vars[$tpl_var]->value[$_mkey] = $_mval;
-                    }
-                } else {
-                    $this->tpl_vars[$tpl_var]->value[] = $value;
-                }
+            } else {
+                $this->tpl_vars[$tpl_var]->value[] = $value;
             }
         }
 
